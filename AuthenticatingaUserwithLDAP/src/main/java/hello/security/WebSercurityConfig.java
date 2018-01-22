c71076ad40e84db4df76c2fc9a853aa7298101fc
dm <cuckopl@gmail.com>
2018-01-22 23:01:16 +0100

    LDAP

--- AuthenticatingaUserwithLDAP/src/main/java/hello/security/WebSercurityConfig.java
+++ AuthenticatingaUserwithLDAP/src/main/java/hello/security/WebSercurityConfig.java
@@ -1,9 +1,11 @@
 package hello.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
     @Override
